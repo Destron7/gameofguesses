6bd3528f1b46856254425f400c4e5cf207fe616d
Destron7 <prodp313@gmail.com>
2024-10-02 14:41:04 +0530

    Initial Commit

---
+++ src/main/java/com/GogApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GogApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/dao/UserEntity.java
@@ -0,0 +1,10 @@
+package com.dao;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="users")
+public class UserEntity {
+	
+}
